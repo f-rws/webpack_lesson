1748eb8a193d656836c693ffc464eead6347c039
Hiroya Furusawa <pogbba.6@gmail.com>
2022-05-21 14:10:06 +0900

    utiletiesファイルを追加

---
+++ src/utilities.js
@@ -0,0 +1,11 @@
+export const getNumberToSquared  = (num) => {
+  return num ** 2;
+}
+
+export const ARSENAL_RW = "ブカヨ";
+
+export default class Lion {
+  static say() {
+    return "Roar";
+  }
+}
