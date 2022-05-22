37ad52ae4ef4cf4d1fba56b578cad96a614c242c
Hiroya Furusawa <pogbba.6@gmail.com>
2022-05-22 20:39:56 +0900

    css-loader と style-loader を適用

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import _ from 'lodash';
 import { getNumberToSquared } from "./utilities";
 import { ARSENAL_RW } from "./utilities";
 import Lion from "./utilities";
+import style from "./style.css";
 
 console.log(getNumberToSquared(3));
 console.log(ARSENAL_RW);
 console.log(Lion.say());
+console.log("style", style);
 
 const component = () => {
   const el = document.createElement('div');
@@ -16,3 +18,4 @@ const component = () => {
 }
 
 document.body.appendChild(component());
+document.body.classList.add("bg")
